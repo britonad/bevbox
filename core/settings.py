4db2edfb61b0e162dc752b92fb7833d56c5ff489
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-02-16 19:02:05 +0200

    📝 Add new fields to settings.

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,7 @@ class Common:
     BABEL_TRANSLATION_DIRECTORIES = os.path.join(BASE_DIR, 'translations')
     BABEL_DEFAULT_LOCALE = 'uk'
     TELEGRAM_API_URL = 'https://api.telegram.org'
+    MAILGUN_DOMAIN_NAME = 'mail.bevbox.com.ua'
     LANGUAGES = {
         'en': 'En',
         'uk': 'Uk',
@@ -37,6 +38,8 @@ class Dev(Common):
     SQLALCHEMY_ECHO = True
     CHANNEL_ID = ''
     BOT_TOKEN = ''
+    MAILGUN_API_KEY = ''
+    EMAIL_RECIPIENT = ''
 
 
 class Prod(Common):
@@ -48,3 +51,5 @@ class Prod(Common):
     SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRESQL_URI')
     CHANNEL_ID = os.environ.get('CHANNEL_ID')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+    EMAIL_RECIPIENT = os.environ.get('EMAIL_RECIPIENT ')
