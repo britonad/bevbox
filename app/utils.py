0f47ade028e3f11221dbdb84f3e5017d1fa06d3f
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-02-16 19:00:35 +0200

    ✨ Add send_email function, change a docstring.

--- app/utils.py
+++ app/utils.py
@@ -2,9 +2,34 @@ import requests
 from flask import current_app
 
 
+def send_email(
+        name: str,
+        email: str,
+        subject: str,
+        message: str
+) -> str:
+    """
+    Sends an e-mail by means of external call of Mailgun API to a recipient
+    from a predefined settings behalf of a sender.
+    """
+
+    return requests.post(
+        'https://api.mailgun.net/v3/{}/messages'.format(
+            current_app.config['MAILGUN_DOMAIN_NAME']
+        ),
+        auth=('api', current_app.config['MAILGUN_API_KEY']),
+        data={
+            'from': '{} <{}>'.format(name, email),
+            'to': current_app.config['EMAIL_RECIPIENT'],
+            'subject': subject,
+            'text': message
+        }
+    ).text
+
+
 def send_message_to_channel(cid: str, text: str) -> dict:
     """
-    Sends a message provided by a text argument to a specified Telegram channel.
+    Sends a message by provided a text argument to a specified Telegram channel.
     """
 
     api_url = '{}/bot{}/sendMessage'.format(
