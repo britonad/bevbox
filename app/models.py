0acddc80a552d4d44714fbc7b4ef52f88bd4ae22
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-06-09 21:33:59 +0300

    📝 Update Order model with new fields.

--- app/models.py
+++ app/models.py
@@ -12,10 +12,16 @@ class Order(db.Model):
     name = db.Column(db.String(256), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     subscription_type = db.Column(db.String(10), nullable=False)
+    payment_option = db.Column(db.String(2), nullable=True)
     city = db.Column(db.String(256), nullable=False)
     department = db.Column(db.Integer, nullable=False)
-    additions = db.Column(db.Boolean, nullable=False)
+    # delivery_option = db.Column(db.String(2), nullable=False)
+    # delivery_address = db.Column(db.String(256), nullable=True)
+    matches = db.Column(db.Boolean, nullable=True)
+    stones = db.Column(db.Boolean, nullable=True)
+    guillotine = db.Column(db.Boolean, nullable=True)
     preferences = db.Column(db.Text, nullable=True)
+    callback = db.Column(db.Boolean, nullable=True)
 
     def __repr__(self):
         return '<Order {}-{}>'.format(self.email, self.id)
