b21f2ffd963f92aa58584d1faa1e85125cefc2f4
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-07-17 23:17:03 +0300

    ✨ Add a global fixtures to be used in all related tests.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,22 @@
 import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+
+from core import create_app
 
 
-@pytest.fixture
-def client():
-    pass
+@pytest.fixture(scope='session')
+def app() -> Flask:
+    """A testing instance of the Flask application."""
+
+    app = create_app()
+    app.testing = True
+
+    return app
+
+
+@pytest.fixture(scope='session')
+def client(app: Flask) -> FlaskClient:
+    """An instance of flask test client."""
+
+    return app.test_client()
